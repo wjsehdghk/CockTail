e1633bc3da3fefd66f75960aca2c517da74c498c
nihil0821 <nihil0821@gmail.com>
2016-04-26 21:48:29 +0900

    Added .gitignore

---
+++ Cocktail_knj/app/src/main/java/com/example/multimedia/cocktail_v000/domain/SharedPreferenceUtil.java
@@ -0,0 +1,7 @@
+package com.example.multimedia.cocktail_v000.domain;
+
+/**
+ * Created by multimedia on 2016-04-26.
+ */
+public class SharedPreferenceUtil {
+}
--- Cocktail_knj/app/src/main/java/com/example/multimedia/cocktail_v000/entity/ResionSettingValues.java
+++
@@ -1,7 +0,0 @@
-package com.example.multimedia.cocktail_v000.entity;
-
-/**
- * Created by multimedia on 2016-04-13.
- */
-public class ResionSettingValues {
-}
