be4047b286505f4bdd26ab96f6f2d8b067d71ec4
Daegeon Noh <nihil0821@gmail.com>
2016-05-22 02:40:47 +0900

    just commit

--- TestCoctail/app/src/main/java/com/mingle/myapplication/ResionCinemaActivity.java
+++ TestCoctail/app/src/main/java/com/mingle/myapplication/ResionCinemaActivity.java
@@ -3,6 +3,7 @@ package com.mingle.myapplication;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.AudioManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,11 +13,14 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.ToggleButton;
 
 import com.mingle.entity.MenuEntity;
@@ -48,7 +52,9 @@ public class ResionCinemaActivity extends AppCompatActivity {
         Bitmap bitmap;
         Bitmap bitmap2;
         Bitmap bitmap3;
-
+        SeekBar seekBar;
+        Switch callServiceSwitchBtn;
+        AudioManager audioManager;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
@@ -166,6 +172,8 @@ public class ResionCinemaActivity extends AppCompatActivity {
                 bitmap.recycle();
                 bitmap2.recycle();
                 bitmap3.recycle();
+                Intent callService = new Intent(this, CallService.class);
+                stopService(callService);
         }
 
         private void setupCustomView() {
@@ -178,71 +186,51 @@ public class ResionCinemaActivity extends AppCompatActivity {
                 mSweetSheet3.setDelegate(customDelegate);
                 mSweetSheet3.setBackgroundEffect(new BlurEffect(8));
                 mSweetSheet3.setBackgroundClickEnable(false);
-        }
-
-        private void setupRecyclerView() {
-
-                final ArrayList<MenuEntity> list = new ArrayList<>();
-                //添加假数据
-                MenuEntity menuEntity1 = new MenuEntity();
-                menuEntity1.iconId = R.drawable.ic_account_child;
-                menuEntity1.titleColor = 0xff96CC7A; //textcolor
-                menuEntity1.title = "code";
-
-
-
-                MenuEntity menuEntity = new MenuEntity();
-                menuEntity.iconId = R.drawable.ic_account_child;
-                menuEntity.titleColor = 0xffb3b3b3;
-                menuEntity.title = "QQ";
-                list.add(menuEntity1);
-                list.add(menuEntity);
-
-
-                // SweetSheet 控件,根据 rl 确认位置
-                mSweetSheet = new SweetSheet(rl);
-
-                //设置数据源 (数据源支持设置 list 数组,也支持从菜单中获取)
-                mSweetSheet.setMenuList(list);
-                //根据设置不同的 Delegate 来显示不同的风格.
-                mSweetSheet.setDelegate(new RecyclerViewDelegate(true));
-                //根据设置不同Effect 来显示背景效果BlurEffect:模糊效果.DimEffect 变暗效果
-                mSweetSheet.setBackgroundEffect(new BlurEffect(8));
-                //设置点击事件
-                mSweetSheet.setOnMenuItemClickListener(new SweetSheet.OnMenuItemClickListener() {
+                view.findViewById(R.id.triToggleButton).setOnClickListener(new View.OnClickListener() {
                         @Override
-                        public boolean onItemClick(int position, MenuEntity menuEntity1) {
-                                //即时改变当前项的颜色
-                                list.get(position).titleColor = 0xff96CC7A;
-                                ((RecyclerViewDelegate) mSweetSheet.getDelegate()).notifyDataSetChanged();
-
-                                //根据返回值, true 会关闭 SweetSheet ,false 则不会.
-                                //Toast.makeText(MainActivity.this, menuEntity1.title + "  " + position, Toast.LENGTH_SHORT).show();
-                                return false;
+                        public void onClick(View v) {
+                                switch (state) {
+                                        case 0:
+                                                audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                                                break;
+                                        case 1:
+                                                audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                                                break;
+                                        case 2:
+                                                audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                                                break;
+                                        default:
+                                                break;
+                                }
                         }
                 });
 
-        }
-
-        private void setupViewpager() {
-
-
-                mSweetSheet2 = new SweetSheet(rl);
-
-                //从menu 中设置数据源
-                mSweetSheet2.setMenuList(R.menu.menu_sweet);
-                mSweetSheet2.setDelegate(new ViewPagerDelegate());
-                mSweetSheet2.setBackgroundEffect(new DimEffect(0.5f));
-                mSweetSheet2.setOnMenuItemClickListener(new SweetSheet.OnMenuItemClickListener() {
+                seekBar =(SeekBar)view.findViewById(R.id.custom_seek);
+                seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
                         @Override
-                        public boolean onItemClick(int position, MenuEntity menuEntity1) {
+                        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                                if (progress < 10) {
+                                        progress = 10;
+                                        seekBar.setProgress(progress);
+                                }
 
-                                //  Toast.makeText(MainActivity.this, menuEntity1.title + "  " + position, Toast.LENGTH_SHORT).show();
-                                return true;
+                                WindowManager.LayoutParams params = getWindow().getAttributes();
+                                params.screenBrightness = (float) progress / 100;
+                                getWindow().setAttributes(params);
+                        }
+
+                        @Override
+                        public void onStartTrackingTouch(SeekBar seekBar) {
+
+                        }
+
+                        @Override
+                        public void onStopTrackingTouch(SeekBar seekBar) {
+
                         }
                 });
-
         }
+
 
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
